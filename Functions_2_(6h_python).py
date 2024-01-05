e9500ac92ddb1c9860096c56ce377763aafd3c59
petr <petr.mouze@gmail.com>
2024-01-05 17:27:49 +0400

    2 part "Functions" was added

---
+++ Functions_2_(6h_python).py
@@ -0,0 +1,31 @@
+"""
+Функции - 2 часть:
+
+Множество аргументов передаваемые по одному.
+Способы передачи аргументов в функцию.
+"""
+
+
+def add_all(*args) -> None:
+    print(args)
+    print(type(args))
+
+
+add_all(1, 2, 3, "text")  # первый способ передачи аргументов в "*args"
+# Результат вывода:
+# (1, 2, 3, 'text')
+# <class 'tuple'> - класс "args" кортеж
+
+
+def total_numbers(*args) -> int:
+    summery = 0
+    for num in args:
+        summery += num
+    return summery
+
+values = [1, 2, 3, 4]
+other_values = [6, 5, 7]
+
+print(total_numbers(*values, *other_values))  # второй способ передачи аргументов в "*args"
+# Результат вывода:
+# 28
