ea123530fe921401c693ec52d078d5cb0c3d3864
petr <petr.mouze@gmail.com>
2024-01-05 12:45:29 +0400

    An empty string was deleted

--- Classes_1_(6h_python).py
+++ Classes_1_(6h_python).py
@@ -11,7 +11,6 @@
 class MyClass:
     pass
 
-
 my_class = MyClass()
 print(type(MyClass))   # run: <class 'type'> - класс
 print(type(my_class))  # run: <class '__main__.MyClass'> - экземпляр класса
